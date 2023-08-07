b466a7823700f18d439999346196d665bdc18258
Trickkyyy <vandark5@gmail.com>
2023-08-07 16:52:59 +0300

    fixing the burger menu so it closes on clickaway,getting to build it for production

--- src/components/BurgerMenu.tsx
+++ src/components/BurgerMenu.tsx
@@ -4,39 +4,48 @@ import { collection } from "../constants";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
 import classes from "./BurgerMenu.module.css";
+import { ClickAwayListener } from "@mui/material";
 
 const BurgerMenu = (): JSX.Element => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const toggleHandler = () => {
     setIsOpen((prev) => !prev);
   };
 
+  const clickAwayHandler = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <nav className={classes.burger_nav}>
-      {isOpen ? (
-        <span onClick={toggleHandler}>
-          <CloseIcon />
-        </span>
-      ) : (
-        <span onClick={toggleHandler}>
-          <MenuIcon />
-        </span>
-      )}
-      <div
-        className={
-          isOpen ? classes.burger_dropdown : classes.burger_dropdown_hidden
-        }
-      >
-        {collection.map(({ title, id }) => (
-          <ul key={id}>
-            <li>
-              <NavLink to={`collection/${title}`}>{title}</NavLink>
-            </li>
-          </ul>
-        ))}
-      </div>
-    </nav>
+    <ClickAwayListener onClickAway={clickAwayHandler}>
+      <nav className={classes.burger_nav}>
+        {isOpen ? (
+          <span onClick={toggleHandler}>
+            <CloseIcon />
+          </span>
+        ) : (
+          <span onClick={toggleHandler}>
+            <MenuIcon />
+          </span>
+        )}
+        <div
+          className={
+            isOpen ? classes.burger_dropdown : classes.burger_dropdown_hidden
+          }
+        >
+          {collection.map(({ title, id }) => (
+            <ul key={id}>
+              <li>
+                <NavLink to={`collection/${title}`} onClick={clickAwayHandler}>
+                  {title}
+                </NavLink>
+              </li>
+            </ul>
+          ))}
+        </div>
+      </nav>
+    </ClickAwayListener>
   );
 };
 
